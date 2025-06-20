188dfc024e9c1c382b705d5c71ecd2a4ea524c4a
Nat Welch <nat@natwelch.com>
2025-06-20 02:29:49 +0000

    docs

--- potential-spork.lua
+++ potential-spork.lua
@@ -1,5 +1,12 @@
 -- potential-spork
 -- v1.0.0 @icco
+--
+-- This script runs a clock that controls six LFOs. Each LFO is slightly offset
+-- from the others. Its frequency is controlled by a connected grid. Each LFO is
+-- set out a different channel to Just Friends via crow.
+--
+-- The clock speed is changed by turning the first encoder.
+-- The clock speed is displayed on the screen.
 
 engine.name = 'PolySub'
 
@@ -13,10 +20,12 @@ end
 
 function enc(n, d)
   -- encoder actions: n = number, d = delta
+  -- This function changes the clock speed of the clock.
 end
 
 function redraw()
   -- screen redraw
+  -- This draws the current clock speed onto the screen.
 end
 
 function cleanup()
